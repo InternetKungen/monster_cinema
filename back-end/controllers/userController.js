a6e2fcf0bdb6e2bbb88f39a02809d0b1805813a7
Roudi22 <roudi.alzkemi@gmail.com>
2024-11-05 11:13:20 +0100

    fix response and add console.log for debugging

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -182,6 +182,7 @@ export const removeTicket = async (req, res) => {
 export const getUserInfo = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).populate("bookings").exec();
+    console.log("User info endpoint hit");
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
@@ -189,7 +190,8 @@ export const getUserInfo = async (req, res) => {
     // Remove the password field from the user object
     const { password, ...userWithoutPassword } = user.toObject();
 
-    res.status(200).json(userWithoutPassword);
+    res.status(200).json({user:userWithoutPassword});
+    console.log("User info sent");
   } catch (error) {
     console.error('Error fetching user info:', error); // Log the error for debugging
     res.status(500).json({ error: 'Server error' });
