72838958155716831e1afe4536e110587928a3b4
Roudi22 <roudi.alzkemi@gmail.com>
2024-11-05 11:12:54 +0100

    delete react import

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 import Footer from './layout/Footer/Footer'
 import Header from './layout/Header/Header'
 import Main from './layout/Main/Main'
+
 // import Background from './components/Background/Background'
 
 const App: React.FC = () => {
-
+  
   return (
     <div className='app'>
       {/* <Background /> */}
