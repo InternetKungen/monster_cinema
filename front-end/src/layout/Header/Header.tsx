f4cacb7e8c34c663c37fdeefe037c138839b3eed
Roudi22 <roudi.alzkemi@gmail.com>
2024-11-05 11:12:20 +0100

    added modal type and logout icon

--- front-end/src/layout/Header/Header.tsx
+++ front-end/src/layout/Header/Header.tsx
@@ -1,24 +1,30 @@
-import React, { useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import SearchIcon from '../../assets/icons/search_35dp_FCAF00_FILL0_wght400_GRAD0_opsz40.png';
-import LoginIcon from '../../assets/icons/person_35dp_FCAF00_FILL0_wght400_GRAD0_opsz40.png';
-import ProfileIcon from "../../assets/icons/clarify_35dp_FCAF00_FILL0_wght400_GRAD0_opsz40.png"
-import { UserContext } from '../../UserContext';
-import './Header.scss';
-import Logo from '../../assets/img/logo-text-side.png';
-import LoginModal from '../../views/modals/LoginModal';
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import LoginIcon from "../../assets/icons/person_35dp_FCAF00_FILL0_wght400_GRAD0_opsz40.png";
+import ProfileIcon from "../../assets/icons/clarify_35dp_FCAF00_FILL0_wght400_GRAD0_opsz40.png";
+import { UserContext } from "../../UserContext";
+import "./Header.scss";
+import Logo from "../../assets/img/logo-text-side.png";
+import LoginModal from "../../views/modals/LoginModal";
+import LogoutIcon from "../../assets/icons/logout_35dp_FCAF00_FILL0_wght400_GRAD0_opsz40.png";
 
 const Header: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
-  const [modalType, setModalType] = useState('login');
-  const handleShow = () => setShowModal(true);
+  const [modalType, setModalType] = useState("login");
+  const handleShow = () => {
+    setModalType("login");
+    setShowModal(true);
+  };
+
   const handleClose = () => setShowModal(false);
   const { user } = useContext(UserContext);
-  console.log(user);
+  if (!user) {
+    console.log("No user");
+  }
   return (
     <header
       className="header position-fixed"
-      style={{ width: '80%', margin: '0 auto' }}
+      style={{ width: "80%", margin: "0 auto" }}
     >
       <div className="container-fluid">
         <div className="row align-items-center">
@@ -41,27 +47,33 @@ const Header: React.FC = () => {
           </div>
 
           <div className="col d-flex justify-content-end">
-            <div className="icon">
-              <Link to="/">
-                <img src={SearchIcon} alt="Search" className="SearchIcon" />
-              </Link>
-            </div>
+            {user && (
+              <div className="icon">
+                <Link to="/profile">
+                  <img src={ProfileIcon} alt="Profile" className="SearchIcon" />
+                </Link>
+              </div>
+            )}
             <div className="icon">
               {user ? (
-                <Link to="/profile">
-                  <img src={ProfileIcon} alt="Login" className="LoginIcon" />
+                <Link to="/">
+                  <img src={LogoutIcon} alt="Login" className="LoginIcon" />
                 </Link>
               ) : (
                 <button onClick={handleShow}>
-                <img src={LoginIcon} alt="Login" className="LoginIcon" />
-              </button>
+                  <img src={LoginIcon} alt="Login" className="LoginIcon" />
+                </button>
               )}
-              
             </div>
           </div>
         </div>
       </div>
-      <LoginModal show={showModal} setModalType={setModalType} type={modalType} handleClose={handleClose} />
+      <LoginModal
+        show={showModal}
+        setModalType={setModalType}
+        type={modalType}
+        handleClose={handleClose}
+      />
     </header>
   );
 };
