0b9b086f914bca962ec79d2171adf2bd87e52622
Roudi22 <roudi.alzkemi@gmail.com>
2024-11-07 10:04:36 +0100

    send an email to the user to update the password with a new random password

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -1,6 +1,15 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 export const userRegister = async (req, res) => {
   try {
@@ -10,14 +19,22 @@ export const userRegister = async (req, res) => {
       return res.status(400).json({ error: "User already exists" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ email, password: hashedPassword, firstName, lastName, role: "user" });
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+      role: "user",
+    });
     await newUser.save();
     const { password: _, ...userResponse } = newUser.toObject();
-    res.status(201).json({ message: "User registered successfully", user: userResponse });
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: userResponse });
   } catch (error) {
     res.status(500).json({ error: "Server error" });
   }
-}
+};
 
 export const userLogin = async (req, res) => {
   try {
@@ -34,18 +51,21 @@ export const userLogin = async (req, res) => {
     if (!token) {
       return res.status(500).json({ error: "Failed to generate token" });
     }
-    res.cookie("token", token, { 
+    res.cookie("token", token, {
       httpOnly: true, // Prevents JavaScript access
-          secure: false, // Set to true in production (over HTTPS)
-          sameSite: 'Lax', // CSRF protection
-          maxAge: 60 * 60 * 1000 // 1 hour
-     });
+      secure: false, // Set to true in production (over HTTPS)
+      sameSite: "Lax", // CSRF protection
+      maxAge: 60 * 60 * 1000, // 1 hour
+    });
     const { password: _, ...userWithoutPassword } = user.toObject();
-    res.status(200).json({ message: "User logged in successfully", user: userWithoutPassword });
+    res.status(200).json({
+      message: "User logged in successfully",
+      user: userWithoutPassword,
+    });
   } catch (error) {
     res.status(500).json({ error: "Server error" });
   }
-}
+};
 
 export const userLogout = (req, res) => {
   try {
@@ -54,24 +74,30 @@ export const userLogout = (req, res) => {
   } catch (error) {
     res.status(500).json({ error: `Server Error ${error.message}` });
   }
-}
+};
 
 export const resetPassword = async (req, res) => {
   try {
-    const { email, oldPassword, newPassword } = req.body;
+    const { email } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-        return res.status(400).json({ error: "User does not exist" });
+      return res.status(400).json({ error: "User does not exist" });
     }
-    const isPasswordValid = await bcrypt.compare(oldPassword, user.password);
-    if (!isPasswordValid) {
-        return res.status(400).json({ error: "Invalid old password" });
-    }
+    const newPassword = Math.random().toString(36).slice(-8);
     const hashedPassword = await bcrypt.hash(newPassword, 10);
-    user.password = hashedPassword;
-    await user.save();
-    res.status(200).json({ message: "Password reset successfully" });
+    await User.findOneAndUpdate({ email }, { password: hashedPassword });
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Ett nytt lösenord har skapats för ditt konto",
+      text: `Your new password is: ${newPassword}`,
+    };
+    await transporter.sendMail(mailOptions);
+
+    res
+      .status(200)
+      .json({ message: "Ett nytt lösenord har skickats till e-post" });
   } catch (error) {
     res.status(500).json({ error: "Server error" });
   }
-}
+};
