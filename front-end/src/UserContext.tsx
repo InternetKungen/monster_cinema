f73b662844d2b8f0cc650aaf7e2a7a5992e59885
Kim Andersson <kandersson@gmail.com>
2024-11-07 14:38:34 +0100

    User page working with bootstrap accordions

--- front-end/src/UserContext.tsx
+++ front-end/src/UserContext.tsx
@@ -1,45 +1,48 @@
-import { createContext, useState, Dispatch, SetStateAction, ReactNode, useEffect } from 'react';
-import { useCookies } from 'react-cookie';
+import {
+  createContext,
+  useState,
+  Dispatch,
+  SetStateAction,
+  ReactNode,
+  useEffect,
+  useMemo,
+} from "react";
+import { useCookies } from "react-cookie";
 interface User {
-    name: string;
-    email: string;
-    // Add other user properties here
+  name: string;
+  email: string;
+  // Add other user properties here
 }
 
 interface UserContextType {
-    user: User | null;
-    setUser: Dispatch<SetStateAction<User | null>>;
+  user: User | null;
+  setUser: Dispatch<SetStateAction<User | null>>;
 }
-
 
 export const UserContext = createContext<UserContextType>({
-    user: null,
-    setUser: () => null // No-op function with the correct type
+  user: null,
+  setUser: () => null, // No-op function with the correct type
 });
 
-const UserProvider = ({children}: {children: ReactNode}) => {
-    const [user, setUser] = useState<User | null>(null);
-    const [cookies] = useCookies(['token']);
-    useEffect(() => {
-      fetch('/api/user/info', {
-        method: 'GET',
-        credentials: 'include',
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.user) {
-            setUser(data.user);
-          } else if (data.error) {
-            console.log(data.error);
-          }
+const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [cookies] = useCookies(["token"]);
+  useEffect(() => {
+    fetch("/api/user/info", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.user) {
+          setUser(data.user);
+        } else if (data.error) {
+          console.log(data.error);
         }
-      )
-    }, [cookies.token])
-  return (
-    <UserContext.Provider value={{user, setUser}}>
-      {children}
-    </UserContext.Provider>
-  )
-}
+      });
+  }, [cookies.token]);
+  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
 
-export default UserProvider
+export default UserProvider;
---
+++ front-end/src/views/auth/Profile/Profile.tsx
@@ -0,0 +1,123 @@
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../../UserContext";
+import Accordion from "react-bootstrap/Accordion";
+// import moment from "moment";
+
+const Profile: React.FC = () => {
+  const { user } = useContext(UserContext);
+  const [bookingHistory, setBookingHistory] = useState<any[]>([]);
+  const [currentBookings, setCurrentBookings] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (user) {
+      fetch("/api/user/tickets", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const today = new Date();
+          setBookingHistory(
+            data.filter(
+              (booking: any) => new Date(booking.bookedAt[0].date) < today
+            )
+          );
+          setCurrentBookings(
+            data.filter(
+              (booking: any) => new Date(booking.bookedAt[0].date) >= today
+            )
+          );
+        });
+    }
+  }, [user]);
+
+  return (
+    <div className="profile-content">
+      <h1>Mina biljetter</h1>
+      <Accordion defaultActiveKey={["0"]} alwaysOpen>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Bokningsthistorik</Accordion.Header>
+          <Accordion.Body>
+            {bookingHistory.length > 0 ? (
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Filmtitel</th>
+                    <th>Bokat datum</th>
+                    <th>Totalt belopp</th>
+                    <th>Biljetttyper</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {bookingHistory.map((booking) => (
+                    <tr key={booking._id}>
+                      <td>{booking.movie.title}</td>
+                      <td>
+                        {new Date(booking.bookedAt[0].date)
+                          .toISOString()
+                          .slice(0, 10)}
+                      </td>
+                      <td>{booking.totalAmount} kr</td>
+                      <td>
+                        {booking.tickets.map((ticket: any) => (
+                          <div key={ticket._id}>
+                            {ticket.quantity} st {ticket.type}
+                          </div>
+                        ))}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p>Inga tidigare bokningar</p>
+            )}
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Aktuella bokningar</Accordion.Header>
+          <Accordion.Body>
+            {currentBookings.length > 0 ? (
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Filmtitel</th>
+                    <th>Bokat datum</th>
+                    <th>Totalt belopp</th>
+                    <th>Biljetttyper</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {currentBookings.map((booking) => (
+                    <tr key={booking._id}>
+                      <td>{booking.movie.title}</td>
+                      <td>
+                        {new Date(booking.bookedAt[0].date)
+                          .toISOString()
+                          .slice(0, 10)}
+                      </td>
+                      <td>{booking.totalAmount} kr</td>
+                      <td>
+                        {booking.tickets.map((ticket: any) => (
+                          <div key={ticket._id}>
+                            {ticket.quantity} st {ticket.type}
+                          </div>
+                        ))}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p>Inga aktuella bokningar</p>
+            )}
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+};
+
+export default Profile;
