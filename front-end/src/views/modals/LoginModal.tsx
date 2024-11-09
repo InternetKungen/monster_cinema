14067d070615451c1a4f33c3e31f818895cb9f8c
Roudi22 <roudi.alzkemi@gmail.com>
2024-11-09 10:51:58 +0100

    remove the login option from the register modal

--- front-end/src/views/modals/LoginModal.tsx
+++ front-end/src/views/modals/LoginModal.tsx
@@ -20,9 +20,9 @@ const LoginModal: React.FC<Props> = ({
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [oldPassword, setOldPassword] = useState("");
-  const [error, setError] = useState('');
-    const [newPassword, setNewPassword] = useState("");
-    const { setUser } = useContext(UserContext);
+  const [error, setError] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const { setUser } = useContext(UserContext);
   useEffect(() => {
     setEmail("");
     setPassword("");
@@ -30,7 +30,7 @@ const LoginModal: React.FC<Props> = ({
     setLastName("");
     setOldPassword("");
     setNewPassword("");
-    setError('');
+    setError("");
 
     if (show) {
       document.body.classList.add("modal-open");
@@ -86,11 +86,11 @@ const LoginModal: React.FC<Props> = ({
       .then((res) => res.json())
       .then((data) => {
         if (data.message) {
-            alert("User logged in successfully");
-            setUser(data.user);
-            handleClose();
+          alert("User logged in successfully");
+          setUser(data.user);
+          handleClose();
         } else {
-            setError(data.error);
+          setError(data.error);
         }
       });
   };
@@ -110,10 +110,10 @@ const LoginModal: React.FC<Props> = ({
       .then((res) => res.json())
       .then((data) => {
         if (data.message) {
-            alert("Password reset successfully");
-            handleClose();
+          alert("Password reset successfully");
+          handleClose();
         } else {
-            setError(data.error);
+          setError(data.error);
         }
       });
   };
@@ -144,23 +144,44 @@ const LoginModal: React.FC<Props> = ({
           <form onSubmit={handleLogin}>
             <div className="form-group">
               <label htmlFor="email">E-post</label>
-              <input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} id="email" name="email" required />
-                {error.includes("User") && <p className="text-danger mt-2 shake">{error}!</p>}
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                id="email"
+                name="email"
+                required
+              />
+              {error.includes("User") && (
+                <p className="text-danger mt-2 shake">{error}!</p>
+              )}
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} id="password" name="password" required />
-                {error.includes("password") && <p className="text-danger mt-2 shake">{error}!</p>}
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id="password"
+                name="password"
+                required
+              />
+              {error.includes("password") && (
+                <p className="text-danger mt-2 shake">{error}!</p>
+              )}
             </div>
             <button type="submit" className="submit-button">
               Logga in
             </button>
             <div className="my-3">
               <p>
-                Har du glömt lösenordet? Klicka <span
-                className="text-decoration-underline pe-auto"
-                onClick={() => setModalType("reset")}
-                >här</span>
+                Har du glömt lösenordet? Klicka{" "}
+                <span
+                  className="text-decoration-underline pe-auto"
+                  onClick={() => setModalType("reset")}
+                >
+                  här
+                </span>
               </p>
             </div>
             <button
@@ -172,62 +193,76 @@ const LoginModal: React.FC<Props> = ({
             </button>
           </form>
         </section>
-      )} 
-      { type === "register" && (
+      )}
+      {type === "register" && (
         <div
-        className="modal-content"
-        style={{
-          background:
-            "linear-gradient(to right, rgba(0,15,38,100), rgba(4,86,133,100), rgba(0,15,38,100))",
-          borderRadius: "10px",
-          padding: "20px",
-          width: "600px",
-          color: "#FCAF00",
-          maxWidth: "90%",
-          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.8)",
-          position: "relative",
-        }}
-      >
-        <span className="close-button" onClick={handleClose}>
-          &times;
-        </span>
-        <h2>Skapa användare</h2>
-        <form onSubmit={handleRegister}>
-          <div className="form-group">
-            <label htmlFor="email">E-post</label>
-            <input
-              type="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              name="email"
-              required
-            />
-            {error && <p className="text-danger mt-2 shake">{error}!</p>}
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Lösenord</label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              name="password"
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="firstname">Förnamn</label>
-            <input type="text" id="firstname" onChange={(e)=>setFirstName(e.target.value)} value={firstName} name="firstname" required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastname">Efternamn</label>
-            <input type="text" onChange={(e)=>setLastName(e.target.value)} value={lastName} id="lastname" name="lastname" required />
-          </div>
-          <button type="submit" className="submit-button">
-            Skapa användare
-          </button>
-          <div className="my-3">
+          className="modal-content"
+          style={{
+            background:
+              "linear-gradient(to right, rgba(0,15,38,100), rgba(4,86,133,100), rgba(0,15,38,100))",
+            borderRadius: "10px",
+            padding: "20px",
+            width: "600px",
+            color: "#FCAF00",
+            maxWidth: "90%",
+            boxShadow: "0 2px 10px rgba(0, 0, 0, 0.8)",
+            position: "relative",
+          }}
+        >
+          <span className="close-button" onClick={handleClose}>
+            &times;
+          </span>
+          <h2>Skapa användare</h2>
+          <form onSubmit={handleRegister}>
+            <div className="form-group">
+              <label htmlFor="email">E-post</label>
+              <input
+                type="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                name="email"
+                required
+              />
+              {error && <p className="text-danger mt-2 shake">{error}!</p>}
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Lösenord</label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                name="password"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="firstname">Förnamn</label>
+              <input
+                type="text"
+                id="firstname"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                name="firstname"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastname">Efternamn</label>
+              <input
+                type="text"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                id="lastname"
+                name="lastname"
+                required
+              />
+            </div>
+            <button type="submit" className="submit-button">
+              Skapa användare
+            </button>
+            {/* <div className="my-3">
             <p>
               Har du redan ett konto? Klicka <span>här</span>
             </p>
@@ -238,16 +273,16 @@ const LoginModal: React.FC<Props> = ({
             className="submit-button"
           >
             Logga in
-          </button>
-        </form>
-      </div>
+          </button> */}
+          </form>
+        </div>
       )}
-    { type === "reset" && (
+      {type === "reset" && (
         <div
-        className="modal-content"
-        style={{
+          className="modal-content"
+          style={{
             background:
-            "linear-gradient(to right, rgba(0,15,38,100), rgba(4,86,133,100), rgba(0,15,38,100))",
+              "linear-gradient(to right, rgba(0,15,38,100), rgba(4,86,133,100), rgba(0,15,38,100))",
             borderRadius: "10px",
             padding: "20px",
             width: "600px",
@@ -255,34 +290,58 @@ const LoginModal: React.FC<Props> = ({
             maxWidth: "90%",
             boxShadow: "0 2px 10px rgba(0, 0, 0, 0.8)",
             position: "relative",
-        }}
+          }}
         >
-        <span className="close-button" onClick={handleClose}>
+          <span className="close-button" onClick={handleClose}>
             &times;
-        </span>
-        <h2>Återställ lösenord</h2>
-        <form onSubmit={handleReset}>
+          </span>
+          <h2>Återställ lösenord</h2>
+          <form onSubmit={handleReset}>
             <div className="form-group">
-            <label htmlFor="email">E-post</label>
-            <input type="email" id="email" onChange={(e)=>setEmail(e.target.value)} value={email} name="email" required />
-            {error.includes("User") && <p className="text-danger mt-2 shake">{error}!</p>}
+              <label htmlFor="email">E-post</label>
+              <input
+                type="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                name="email"
+                required
+              />
+              {error.includes("User") && (
+                <p className="text-danger mt-2 shake">{error}!</p>
+              )}
             </div>
             <div className="form-group">
-            <label htmlFor="oldpassword">Gammalt lösenord</label>
-            <input type="password" id="oldpassword" onChange={(e)=>setOldPassword(e.target.value)} value={oldPassword} name="oldpassword" required />
-            {error.includes("password") && <p className="text-danger mt-2 shake">{error}!</p>}
+              <label htmlFor="oldpassword">Gammalt lösenord</label>
+              <input
+                type="password"
+                id="oldpassword"
+                onChange={(e) => setOldPassword(e.target.value)}
+                value={oldPassword}
+                name="oldpassword"
+                required
+              />
+              {error.includes("password") && (
+                <p className="text-danger mt-2 shake">{error}!</p>
+              )}
             </div>
             <div className="form-group">
-            <label htmlFor="newpassword">Nytt lösenord</label>
-            <input type="password" id="newpassword" onChange={(e)=>setNewPassword(e.target.value)} value={newPassword} name="newpassword" required />
+              <label htmlFor="newpassword">Nytt lösenord</label>
+              <input
+                type="password"
+                id="newpassword"
+                onChange={(e) => setNewPassword(e.target.value)}
+                value={newPassword}
+                name="newpassword"
+                required
+              />
             </div>
             <button type="submit" className="submit-button">
-            Återställ lösenord
+              Återställ lösenord
             </button>
-        </form>
+          </form>
         </div>
-    )}
-      
+      )}
     </div>
   );
 };
