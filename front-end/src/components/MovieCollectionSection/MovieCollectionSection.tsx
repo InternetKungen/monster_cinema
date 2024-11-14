42798076a063b87e0a0fc85fdf01c033a6d69f9c
Kim Andersson <kandersson@gmail.com>
2024-11-14 17:31:14 +0100

    added wrapper to movie-grid and made a fade out border

--- front-end/src/components/MovieCollectionSection/MovieCollectionSection.tsx
+++ front-end/src/components/MovieCollectionSection/MovieCollectionSection.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import MovieComponent from '../MovieComponent/MovieComponent';
-import Button from '../FrontPageButton/FrontPageButton';
-import TitleBarComponent from '../TitleBarComponent/TitleBarComponent';
-import './MovieCollectionSection.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from "react";
+import MovieComponent from "../MovieComponent/MovieComponent";
+import Button from "../FrontPageButton/FrontPageButton";
+import TitleBarComponent from "../TitleBarComponent/TitleBarComponent";
+import "./MovieCollectionSection.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 interface Movie {
   _id: string;
@@ -20,16 +20,16 @@ const MovieCollectionSection: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [filterField, setFilterField] = useState<string | null>(null);
-  const [filterValue, setFilterValue] = useState<string | number |  null>(null);
+  const [filterValue, setFilterValue] = useState<string | number | null>(null);
   const [isMinThreshold, setIsMinThreshold] = useState<boolean>(false);
   const [showMore, setShowMore] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const response = await fetch('/api/movie');
+        const response = await fetch("/api/movie");
         if (!response.ok) {
-          throw new Error('Failed to fetch movies');
+          throw new Error("Failed to fetch movies");
         }
         const data = await response.json();
         setMovies(data);
@@ -47,13 +47,17 @@ const MovieCollectionSection: React.FC = () => {
   if (error) return <div>Error: {error}</div>;
 
   const handleFilter = (field: string | null, value: string | null) => {
-	console.log("Filter / Value: " + field + " : " + value)
+    console.log("Filter / Value: " + field + " : " + value);
     setFilterField(field);
     setFilterValue(value);
-	console.log("Filter / Value: " + field + " : " + value)
+    console.log("Filter / Value: " + field + " : " + value);
   };
 
-  const handleNumericFilter = (field: string | null, threshold: number | null, minThreshold: boolean = false) => {
+  const handleNumericFilter = (
+    field: string | null,
+    threshold: number | null,
+    minThreshold: boolean = false
+  ) => {
     console.log("Numeric Filter / Field: " + field + " : " + threshold);
     setFilterField(field);
     setFilterValue(threshold);
@@ -62,64 +66,93 @@ const MovieCollectionSection: React.FC = () => {
 
   const itemsPerRow = 3;
   const initialMoviesToShow = 5;
-  const filteredMovies = filterField && filterValue
-    ? movies.filter((movie) => {
-        const fieldValue = movie[filterField as keyof Movie];
+  const filteredMovies =
+    filterField && filterValue
+      ? movies.filter((movie) => {
+          const fieldValue = movie[filterField as keyof Movie];
 
-        if (typeof filterValue === "number" && typeof fieldValue === "number") {
-          return isMinThreshold ? fieldValue >= filterValue : fieldValue < filterValue;
-        }
+          if (
+            typeof filterValue === "number" &&
+            typeof fieldValue === "number"
+          ) {
+            return isMinThreshold
+              ? fieldValue >= filterValue
+              : fieldValue < filterValue;
+          }
 
-        if (typeof filterValue === "string") {
-          return Array.isArray(fieldValue)
-          ? fieldValue.includes(filterValue)
-          : fieldValue === filterValue;
-        }
-    return false;
-	})
-    : movies;
+          if (typeof filterValue === "string") {
+            return Array.isArray(fieldValue)
+              ? fieldValue.includes(filterValue)
+              : fieldValue === filterValue;
+          }
+          return false;
+        })
+      : movies;
 
-	const displayedMovies = showMore
-	? filteredMovies
-	: filteredMovies.slice(0, Math.ceil(initialMoviesToShow / itemsPerRow) * itemsPerRow);
+  const displayedMovies = showMore
+    ? filteredMovies
+    : filteredMovies.slice(
+        0,
+        Math.ceil(initialMoviesToShow / itemsPerRow) * itemsPerRow
+      );
 
-
-	console.log("Filetered: " + JSON.stringify(filteredMovies))
+  console.log("Filetered: " + JSON.stringify(filteredMovies));
   return (
     <div className="container col-12 py-5">
       <section className="movie-collection-section col-12 g-0">
-		<TitleBarComponent className="titlebar-component" title='Våra filmer' />
+        <TitleBarComponent className="titlebar-component" title="Våra filmer" />
         <div className="sorting-button-container text-center col-md-12 col-lg-6 g-0">
-          <Button className="filter-button" text="Alla Filmer" onClick={() => handleFilter(null, null)} />
-          <Button className="filter-button" text="Barn & Familj" onClick={() => handleNumericFilter('ageRestriction', 15)} />
-          <Button className="filter-button" text="Senaste" onClick={() => handleFilter('year', '2024')} />
-          <Button className="filter-button" text="Populära" onClick={() => handleNumericFilter('imdbRating', 7.5, true)} />
-          <Button className="filter-button" text="Klassiker" onClick={() => handleNumericFilter('year', 1995)} />
+          <Button
+            className="filter-button"
+            text="Alla Filmer"
+            onClick={() => handleFilter(null, null)}
+          />
+          <Button
+            className="filter-button"
+            text="Barn & Familj"
+            onClick={() => handleNumericFilter("ageRestriction", 15)}
+          />
+          <Button
+            className="filter-button"
+            text="Senaste"
+            onClick={() => handleFilter("year", "2024")}
+          />
+          <Button
+            className="filter-button"
+            text="Populära"
+            onClick={() => handleNumericFilter("imdbRating", 7.5, true)}
+          />
+          <Button
+            className="filter-button"
+            text="Klassiker"
+            onClick={() => handleNumericFilter("year", 1995)}
+          />
         </div>
 
-        {/* <div className="row g-0"> */}
+        <div className="movie-grid-container">
           <div className="movie-grid col-12">
-          {displayedMovies.map((movie) => (
-            <div key={movie._id} className="movie-item">
-              <MovieComponent
-                _id={movie._id}
-                title={movie.title}
-                year={movie.year}
-                poster={movie.poster}
-                genre={movie.genre}
-                ageRestriction={movie.ageRestriction}
-              />
-            </div>
-          ))}
+            {displayedMovies.map((movie) => (
+              <div key={movie._id} className="movie-item">
+                <MovieComponent
+                  _id={movie._id}
+                  title={movie.title}
+                  year={movie.year}
+                  poster={movie.poster}
+                  genre={movie.genre}
+                  ageRestriction={movie.ageRestriction}
+                />
+              </div>
+            ))}
           </div>
-		   <div className="text-center mt-3">
+        </div>
+        <div className="text-center mt-3">
           <Button
             className="hidden-button"
             text={showMore ? "Visa färre filmer" : "Visa fler filmer"}
             onClick={() => setShowMore(!showMore)}
           />
         </div>
-          {/* </div> */}
+        {/* </div> */}
       </section>
     </div>
   );
